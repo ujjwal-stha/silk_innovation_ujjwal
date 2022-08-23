c9921a9f3dfb52b0f6350b355b6d41199fbb9957
ujjwal-stha <shrestha19ujjwal@gmail.com>
2022-08-23 11:24:57 +0545

    update handleChange  and inputfield values and cleanup

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,19 +3,26 @@ import React, { useState, useEffect } from "react";
 const Login = () => {
   const initialValues = { email: "", phone: "", password: "", pin: "" };
   const [formValues, setFormValues] = useState(initialValues);
+  //const [userInput, setUserInput] = useState("");
   const [formErrors, setFormErrors] = useState("");
   const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+  const handleUserChange = (e) => {
+    //const { name, value } = e.target;
+    //setFormValues({ ...formValues, [name]: value });
+    const userInput = e.target.value;
+    const isUser = validate(userInput);
+    if (isUser) setFormValues({ ...formValues, email: userInput, phone: "" });
+    else setFormValues({ ...formValues, phone: userInput, email: "" });
   };
 
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     setFormErrors(validate(formValues));
-  //     setIsSubmit(true);
-  //   };
+  const handlePasswordChange = (e) => {
+    const userInput = e.target.value;
+    const isPassword = validate(userInput);
+    if (isPassword)
+      setFormValues({ ...formValues, password: userInput, pin: "" });
+    else setFormValues({ ...formValues, pin: userInput, password: "" });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,7 +31,7 @@ const Login = () => {
       method: "POST",
       headers: {
         "App-Authorizer": 647061697361,
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -39,63 +46,12 @@ const Login = () => {
     console.log(json);
     if (json.success) {
       localStorage.setItem("token", json.access_token);
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-      
-    //   setFormErrors(validate(formValues));
-    if (formValues.email === initialValues.email || formValues.phone === initialValues.phone) {
-        if (formValues.password === initialValues.password || formValues.pin === initialValues.pin) {
-          console.log("User Loged In");
-        } else {
-          console.log("wrong credentials");
-        }
-      } else {
-        console.log("please check you username or Email");
-      }
-
-
-      if (!formValues.email || !formValues.phone) {
-        console.log("Email or Phone  is required!");
-      } else if (!regex.test(formValues.email)) {
-        console.log("This is not a valid email format!");
-      } else if (formValues.phone !== 10) {
-        console.log("The length should be 10 digits!");
-      } else if (formValues.phone !== 984 || formValues.phone !== 986) {
-        console.log("The NTC prepaid should start with 984 or 986 digits!");
-      } else if (formValues.phone !== 985) {
-        console.log("The NTC postpaid should start with 985 digits!");
-      } else if (formValues.phone !== 974 || formValues.phone !== 975) {
-        console.log("The NTC CDMA should start with 974 or 975 digits!");
-      } else if (
-        formValues.phone !== 980 ||
-        formValues.phone !== 981 ||
-        formValues.phone !== 982
-      ) {
-        console.log("The NCell should start with 980 or 981 or 982 digits!");
-      } else if (formValues.phone !== 988 || formValues.phone !== 962) {
-        console.log("The SMARTCELL should start with 988 or 962");
-      } else if (formValues.phone !== 972) {
-        console.log("The UTL should start with 972");
-      }
-
-
-      if (!formValues.password || !formValues.pin) {
-        console.log("Password or Pin is required!");
-      } else if (formValues.pin.length < 3) {
-        console.log("Pin must be more than 3 characters");
-      } else if (formValues.password.length < 8) {
-        console.log("Password  must be more than 8 characters");
-      }
-      
-    
-
+      setFormErrors(validate(formValues));
       setIsSubmit(true);
       alert("Logged in  Successfully");
     } else {
       alert("Invaild Credentials");
     }
-
-    
-
   };
 
   useEffect(() => {
@@ -105,59 +61,59 @@ const Login = () => {
     }
   }, [formErrors]);
 
-//   const validate = (values) => {
-//     const errors = {};
-//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-//     if (
-//       values.email === initialValues.email ||
-//       values.phone === initialValues.phone
-//     ) {
-//       if (
-//         values.password === initialValues.password ||
-//         values.phone === initialValues.pin
-//       ) {
-//         console.log("user logged in");
-//       } else {
-//         console.log("Invalid data");
-//       }
-//     }
+    if (
+      values.email === formValues.email ||
+      values.phone === formValues.phone
+    ) {
+      if (
+        values.password === formValues.password ||
+        values.pin === formValues.pin
+      ) {
+        console.log("user logged in");
+      } else {
+        console.log("Invalid data");
+      }
+    }
 
-//     if (!values.email || !values.phone) {
-//       errors.email = "Email  is required!";
-//       errors.phone = "Phone no is required";
-//     } else if (!regex.test(values.email)) {
-//       errors.email = "This is not a valid email format!";
-//     } else if (values.phone !== 10) {
-//       errors.phone = "The length should be 10 digits!";
-//     } else if (values.phone !== 984 || values.phone !== 986) {
-//       errors.phone = "The NTC prepaid should start with 984 or 986 digits!";
-//     } else if (values.phone !== 985) {
-//       errors.phone = "The NTC postpaid should start with 985 digits!";
-//     } else if (values.phone !== 974 || values.phone !== 975) {
-//       errors.phone = "The NTC CDMA should start with 974 or 975 digits!";
-//     } else if (
-//       values.phone !== 980 ||
-//       values.phone !== 981 ||
-//       values.phone !== 982
-//     ) {
-//       errors.phone = "The NCell should start with 980 or 981 or 982 digits!";
-//     } else if (values.phone !== 988 || values.phone !== 962) {
-//       errors.phone = "The SMARTCELL should start with 988 or 962";
-//     } else if (values.phone !== 972) {
-//       errors.phone = "The UTL should start with 972";
-//     }
+    if (!values.email || !values.phone) {
+      errors.email = "Email  is required!";
+      errors.phone = "Phone no is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    } else if (values.phone !== 10) {
+      errors.phone = "The length should be 10 digits!";
+    } else if (values.phone !== 984 || values.phone !== 986) {
+      errors.phone = "The NTC prepaid should start with 984 or 986 digits!";
+    } else if (values.phone !== 985) {
+      errors.phone = "The NTC postpaid should start with 985 digits!";
+    } else if (values.phone !== 974 || values.phone !== 975) {
+      errors.phone = "The NTC CDMA should start with 974 or 975 digits!";
+    } else if (
+      values.phone !== 980 ||
+      values.phone !== 981 ||
+      values.phone !== 982
+    ) {
+      errors.phone = "The NCell should start with 980 or 981 or 982 digits!";
+    } else if (values.phone !== 988 || values.phone !== 962) {
+      errors.phone = "The SMARTCELL should start with 988 or 962";
+    } else if (values.phone !== 972) {
+      errors.phone = "The UTL should start with 972";
+    }
 
-//     if (!values.password || !values.pin) {
-//       errors.password = "Password is required!";
-//       errors.pin = "pin is required";
-//     } else if (values.pin.length < 3) {
-//       errors.password = "Pin must be more than 3 characters";
-//     } else if (values.password.length < 8) {
-//       errors.password = "Password  must be more than 8 characters";
-//     }
-//     return errors;
-//   };
+    if (!values.password || !values.pin) {
+      errors.password = "Password is required!";
+      errors.pin = "pin is required";
+    } else if (values.pin.length < 4) {
+      errors.pin = "Pin must be more than 4 characters";
+    } else if (values.password.length < 6) {
+      errors.password = "Password  must be more than 6 characters";
+    }
+    return errors;
+  };
 
   return (
     <div className="container">
@@ -176,10 +132,10 @@ const Login = () => {
             <label>Email/Phone:</label>
             <input
               type="text"
-              name="email"
+              //name="email"
               placeholder="Email/Phone"
-              value={formValues.email}
-              onChange={handleChange}
+              value={formValues.email || formValues.phone}
+              onChange={handleUserChange}
             />
           </div>
           <p>{formErrors.email}</p>
@@ -188,10 +144,10 @@ const Login = () => {
             <label>Password/Pin:</label>
             <input
               type="password"
-              name="password"
+              //name="password"
               placeholder="Password/Pin"
-              value={formValues.password}
-              onChange={handleChange}
+              value={formValues.password || formValues.pin}
+              onChange={handlePasswordChange}
             />
           </div>
           <p>{formErrors.password}</p>
